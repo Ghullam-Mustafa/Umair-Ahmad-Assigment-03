70cb29ce3a373044ecf22fd7530bc529de8f6f35
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-05-05 22:59:54 +0500

    add js to clear input feild

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// clear input field
+document.getElementById('erase').onclick = function () {
+    document.getElementById('data').value = ''
+}
+// output clear
+document.getElementById('Clean').onclick = function () {
+    document.getElementById('output').innerHTML = ''
+}
